993ac0d36629e8e50fd227072cac620a970e6686
sodomak <cancel@cancel.cz>
2018-08-06 17:58:25 +0200

    Update fileslicer.py
    Added different first line

--- fileslicer.py
+++ fileslicer.py
@@ -1,17 +1,17 @@
 import urllib.parse
 
-path = "./test.txt"
+path = "./sh.ps1"
 file = open(path, "r")
 
-maxOutchar = 6
+maxOutchar = 100
 outLine = ""
 output = ""
-outFile = "./out.txt"
+outFile = "./a.txt"
 urlConvert = True
-prepend = "echo "
-append = " >> test.ps1"
+prepend = "@echo "
+append = " >> c:\\Users\\merlin\\AppData\\Local\\Temp\\vmware-merlin\\VMwareDnD\\d7c161a1\\UploadedFiles\\sodomak\\shell.ps1" #TODO: Add bckslashes on input
+append1st = " > c:\\Users\\merlin\\AppData\\Local\\Temp\\vmware-merlin\\VMwareDnD\\d7c161a1\\UploadedFiles\\sodomak\\shell.ps1"
 urlConverAll = True
-firstLine = True # TODO: Different first line >
 """
 if urlConverAll:
     prepend = urllib.parse.quote_plus(prepend)
@@ -21,26 +21,29 @@ if urlConverAll:
 with file as f:
 
     for line in f:
-#        print("line: " + line)
         chunksNr = (len(line)//maxOutchar)+1
         i = 0
         j = 1
         eol = ""
         for chunks in range(0,chunksNr):
             if j == chunksNr:
-                eol = "\\n"
-            outLine = prepend + line[i+chunks:(i+chunks)+maxOutchar+1].rstrip() + eol + append
+                eol = ""
+            if firstLine:
+                outLine = prepend + line[i+chunks:(i+chunks)+maxOutchar+1].rstrip() + eol + append1st
+            else:	
+                outLine = prepend + line[i+chunks:(i+chunks)+maxOutchar+1].rstrip() + eol + append
+            firstLine = False
             if urlConverAll:
                 outLine = urllib.parse.quote_plus(outLine)
-            print(outLine)
+ #           print(outLine)
             j += 1
             i +=maxOutchar
             outLine += "\n"
 
             output += outLine
- #           print("Chunk: ")
 
 with open(outFile, 'a') as out:
     out.write(output)
 
-print((10//4)+1)
+
+# TODO: Add all txt's in the path
